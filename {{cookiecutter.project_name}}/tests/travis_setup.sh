d0af9654916355d5b8d4406d000112bdb06cf14e
eliavw <elia.vw@gmail.com>
2019-08-13 16:52:56 +0200

    Travis should now work

--- {{cookiecutter.project_name}}/tests/travis_setup.sh
+++ {{cookiecutter.project_name}}/tests/travis_setup.sh
@@ -27,12 +27,13 @@ conda update --yes conda
 # Create a local environment from the environment.yml file.
 
 echo "Recreate conda environment in Travis servers"
-conda env create -p ./.conda -f dependencies-deploy.yaml
+conda env create -p ./.conda -f dependencies-deploy.yaml -n {{cookiecutter.project_name}}
 echo "Recreation done"
 
 # Conda activate does not work, use source instead.
 echo "Activating new env now"
 source activate ./.conda
+conda env update -n {{cookiecutter.project_name}} -f dependencies-develop.yaml
 
 travis-cleanup() {
     printf "Cleaning up environments ... "  # printf avoids new lines
