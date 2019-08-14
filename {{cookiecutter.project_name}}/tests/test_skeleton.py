91125833e0ce2d2886ca6e25a3a7dbd8ad3b3f1e
eliavw <elia.vw@gmail.com>
2019-08-14 11:01:09 +0200

    Added demo files

---
+++ {{cookiecutter.project_name}}/tests/test_skeleton.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+from {{cookiecutter.project_name}}.skeleton import fib
+
+__author__ = {{cookiecutter.author_name}}
+__copyright__ = {{cookiecutter.author_name}}
+__license__ = {{cookiecutter.license}}
+
+
+def test_fib():
+    assert fib(1) == 1
+    assert fib(2) == 1
+    assert fib(7) == 13
+    with pytest.raises(AssertionError):
+        fib(-10)
