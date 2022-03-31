3f7c3cf979f408c0fc5ca000a98f4223fffb8bf7
Elia Van Wolputte <elia.vw@gmail.com>
2022-03-31 12:17:14 +0200

    better test suite

---
+++ {{cookiecutter.project_name}}/tests/test_pytest.py
@@ -0,0 +1,9 @@
+"""
+This file contains a trivial always-passing test. This ensures 
+that gh-actions and PyTest itself are working properly.
+"""
+
+
+def test_placeholder():
+    # Trivial test to assert that PyTest itself works properly.
+    assert True
